4cb61fcc1446bc9e5f9c26956a1fd5d773432cbe
Matheus Maidel <matheus.maidel@gmail.com>
2024-09-15 13:49:59 -0300

    Rename feats to endpoints.

--- api/src/authentic/authentic.module.ts
+++ api/src/authentic/authentic.module.ts
@@ -1,13 +1,13 @@
 import {Module} from "@nestjs/common";
-import {SignUpController} from "./feats/signup/signup.controller";
-import {SignUpService} from "./feats/signup/signup.service";
+import {SignUpController} from "./endpoints/signup/signup.controller";
+import {SignUpService} from "./endpoints/signup/signup.service";
 import {DatabaseModule} from "src/database/database.module";
 import {HashModule} from "src/hash/hash.module";
 import {TokenModule} from "src/token/token.module";
-import {LogInController} from "./feats/login/login.controller";
-import {LogInService} from "./feats/login/login.service";
-import {LogOutController} from "./feats/logout/logout.controller";
-import {LogOutService} from "./feats/logout/logout.service";
+import {LogInController} from "./endpoints/login/login.controller";
+import {LogInService} from "./endpoints/login/login.service";
+import {LogOutController} from "./endpoints/logout/logout.controller";
+import {LogOutService} from "./endpoints/logout/logout.service";
 
 @Module({
 	imports: [DatabaseModule, HashModule, TokenModule],
